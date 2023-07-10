1e86b015d55c030dcb3506069bf7aaa9ec271777
Ian Leatherbury <ian.leatherbury@gmail.com>
2023-07-09 19:30:35 -0700

    try other model

--- src/pages/api/openai-chat.ts
+++ src/pages/api/openai-chat.ts
@@ -33,7 +33,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       const combinedMessages = [...defaultMessages, ...incomingMessages];
 
       const completion = await openai.createChatCompletion({
-        model: "gpt-4-32k",
+        model: "gpt-4-0613",
         messages: combinedMessages,
       });
 
